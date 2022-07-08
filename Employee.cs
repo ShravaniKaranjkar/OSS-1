a474263e3606730700dc64673c326ec371336cee
ShitalShivarkar <108930327+ShitalShivarkar@users.noreply.github.com>
2022-07-08 15:52:52 +0530

    Added Employee Source code

---
+++ Employee.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestProject
+{
+    public   partial class Employee:ICloneable
+    {
+
+        public Employee()
+        {
+
+        }
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public virtual double CalculateSalary()
+        { 
+            return BasicSalary +5000 + DailyAllowance;
+        }
+
+        public object Clone()
+        {
+           Employee employee=new Employee();
+            employee.FirstName=this.FirstName;
+            employee.LastName=this.LastName;
+            return employee;
+
+        }
+
+
+        ~Employee()
+        {
+
+            int count = 56;
+            count++;
+            //DeInitialization Code
+
+        }
+    }
+}
